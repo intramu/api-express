a19f9fbd8c9564d0965679a4fc691708c7450840
Noah Roerig <noahr1936@gmail.com>
2022-10-12 01:16:42 -0700

    Small changes

--- src/data/adminDAO.ts
+++ src/data/adminDAO.ts
@@ -4,10 +4,16 @@ import { Organization } from "../models/Organization";
 import logger from "../utilities/winstonConfig";
 
 const pool = mysql2.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    // port:3306
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "intramu",
+    port:3306
 });
 
 export default class adminDAO {
@@ -39,14 +45,16 @@ export default class adminDAO {
 
         let conn = null;
         let sqlCreateOrg =
-            "INSERT INTO organization (ID, NAME, INFO, IMAGE, MAIN_COLOR, APPROVAL_STATUS, DATE_CREATED) VALUES (UNHEX(REPLACE('8418a13e-2bb1-42cf-8736-a3e6d82a4eef','-',''))),?,?,?,?,?,?)";
+            // "INSERT INTO organization (ID, NAME, INFO, IMAGE, MAIN_COLOR, APPROVAL_STATUS, DATE_CREATED) VALUES (?,?,?,?,?,?,?)";
 
+        // let sqlCreateOrg =
+            "INSERT INTO organization (ID, NAME, INFO, IMAGE, MAIN_COLOR, APPROVAL_STATUS, DATE_CREATED) VALUES (UUID_TO_BIN('6ccd780c-baba-1026-9564-5b8c656024db'),?,?,?,?,?,?)";
         let sqlCreateMasterAdmin =
             "INSERT INTO admin (AUTH_ID, FIRST_NAME, LAST_NAME, LANGUAGE, ROLE, DATE_CREATED, organization_ID) VALUES (?,?,?,?,?,?,?) ";
 
         try {
             conn = await pool.getConnection();
-            let [resultCreateOrg, fields] = await conn.query(sqlCreateOrg, [
+            let [resultCreateOrg, fields]  = await conn.query(sqlCreateOrg, [
                 org.getName(),
                 org.getInfo(),
                 org.getImage(),
@@ -56,6 +64,7 @@ export default class adminDAO {
             ]);
 
             console.log(resultCreateOrg);
+            await conn.commit()
         } catch (error) {
             if (conn) await conn.rollback();
 
@@ -70,11 +79,40 @@ export default class adminDAO {
         }
     }
 
-    async findAllOrganization() {}
+    async findAllOrganizations() {
+        logger.verbose("Entering method findAllOrganizations()", {
+            class: this.className,
+        });
+        
+        let conn = null;
+
+        let sqlViewAll = "SELECT * FROM organization"
+
+        try {
+            conn = await pool.getConnection()
+            let [resultViewAll, fields] = await conn.query(sqlViewAll)
+
+            console.log(resultViewAll);
+            // console.log(fields);            
+            return resultViewAll
+        } catch (error) {
+            if(conn) await conn.rollback()
+            return null
+        }finally{
+            if(conn) conn.release()
+        }
+    }
 
     async deleteOrganization() {}
 
-    async updateOrganization() {}
+    async updateOrganization(org: Organization) {
+        logger.verbose("Entering method updateOrganization()", {
+            class: this.className,
+        });
+
+        let conn = null;
+        let sqlUpdateOrg = "UPDATE organization SET NAME = ?, INFO = ?, IMAGE = ?, MAIN_COLOR = ?, APPROVAL_STATUS = ?"
+    }
 
     async createAdmin() {}
 
@@ -82,3 +120,11 @@ export default class adminDAO {
 
     async deleteAdmin() {}
 }
+
+let test = new adminDAO();
+
+console.log("hello");
+
+
+// test.createOrganization(new Organization("2", "GCU", "none", "Christian", "Purple", "UNAPPROVED", new Date()))
+test.findAllOrganizations()
